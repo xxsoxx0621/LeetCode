de2642317fba0630cdbd73256013eaead168fb0d
so <45780936+xxsoxx0621@users.noreply.github.com>
2023-03-09 09:37:10 +0900

    Time: 53 ms (96.43%), Space: 42.4 MB (40.65%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+   let arr = [];
+   for(let i=0; i < nums.length; i++){
+       if(i === 0){
+           arr.push(nums[0]);
+           
+       }else{
+         arr.push(arr[i-1]+ nums[i]);   
+       }
+   }
+
+   return arr;
+};
