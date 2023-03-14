028aa67b70a2bdbcc66c2ae18d33d200547de2e5
sohyun <45780936+xxsoxx0621@users.noreply.github.com>
2023-03-14 14:54:51 +0900

    Time: 54 ms (99.79%), Space: 44.8 MB (34.29%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var pivotIndex = function(nums) {
+    
+    let totalSum = 0;
+    let leftSum = 0
+    
+    nums.forEach((element) => totalSum += element);
+    
+    for(let i=0; i < nums.length; i++){
+        if(totalSum - leftSum - nums[i] === leftSum){
+            return i;
+        }
+        leftSum += nums[i]
+    }
+    return -1;
+};  
